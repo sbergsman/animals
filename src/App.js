a39386af929a26b5cb3e578a21f20dc9b1f4e8b5
steve <steve.bergsman@gmail.com>
2024-10-21 07:58:08 -0500

    52 fun with animal state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { useState } from "react";
+
+function getRandomAnimal(){
+    const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
+    return animals[Math.floor(Math.random() * animals.length)];
+}
+
 function App() {
-    return <div>Show Animal list here</div>;
+
+    const [animals, setAnimals] = useState([]);
+    
+    const handleClick = () => {
+        setAnimals([...animals, getRandomAnimal()]); 
+    };
+
+    return (
+        <div>
+            <button onClick={handleClick}>Add animal</button>
+            <div>
+                {animals}
+            </div>
+        </div>
+    );
 }
 
 export default App;
